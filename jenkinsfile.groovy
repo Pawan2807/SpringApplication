9f23291ade4daf72ab37ef8c8aed12fd3206aa5c
Pawan Deep <Pawan.Deep@Geminisolutions.com>
2025-03-24 16:40:43 +0530

    Added jenkins file

---
+++ jenkinsfile.groovy
@@ -0,0 +1,25 @@
+pipeline {
+agent any
+stages {
+stage('Build') {
+steps {
+echo "build completed successfully"
+}
+}
+stage('Test') {
+steps {
+echo "test completed successfully"
+}
+}
+stage('Package') {
+steps {
+echo "package completed successfully"
+}
+}
+stage('Deploy') {
+steps {
+echo "deploy completed successfully"
+}
+}
+}
+}
